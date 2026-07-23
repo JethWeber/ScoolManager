46a5e6360adbe4335edff7094e450bd18669fe87
Jeth Weber <jethweber@gmail.com>
2026-07-23 22:43:02 +0100

    criacao e conclusaoda tele de detalhes do aluno

--- ScoolManager.Desktop/ViewModels/MainWindowViewModel.cs
+++ ScoolManager.Desktop/ViewModels/MainWindowViewModel.cs
@@ -1,3 +1,9 @@
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Material.Icons;
+using ScoolManager.Desktop.Models;
+using ScoolManager.Desktop.ViewModels.Pages;
 
 namespace ScoolManager.Desktop.ViewModels;
 
@@ -27,7 +33,7 @@ public partial class MainWindowViewModel : ViewModelBase
         NavigationItems = new ObservableCollection<NavigationItemViewModel>
         {
             new(MaterialIconKind.ViewDashboard, "Dashboard",      () => new DashboardViewModel()),
-            new(MaterialIconKind.AccountGroup,  "Alunos",         () => new AlunosViewModel()), // TODO: AlunosViewModel
+            new(MaterialIconKind.AccountGroup,  "Alunos",         CriarPaginaAlunos),
             new(MaterialIconKind.CashMultiple,  "Financeiro",     () => new DashboardViewModel()), // TODO: FinanceiroViewModel
             new(MaterialIconKind.ChartLine,     "Relatórios",     () => new DashboardViewModel()), // TODO: RelatoriosViewModel
             new(MaterialIconKind.Domain,        "Escola",         () => new DashboardViewModel()), // TODO: EscolaViewModel
@@ -36,6 +42,36 @@ public partial class MainWindowViewModel : ViewModelBase
 
         _selectedNavigationItem = NavigationItems[0];
         _currentPage = _selectedNavigationItem.PageFactory();
+    }
+
+    /// <summary>
+    /// Cria a AlunosViewModel já com a navegação para "Detalhes do Aluno" ligada:
+    /// ao clicar numa linha (evento DetalhesAlunoSolicitado), trocamos o
+    /// CurrentPage para a DetalhesAlunoViewModel correspondente.
+    /// </summary>
+    private ViewModelBase CriarPaginaAlunos()
+    {
+        var alunosViewModel = new AlunosViewModel();
+        alunosViewModel.DetalhesAlunoSolicitado += (_, aluno) => AbrirDetalhesAluno(aluno);
+        return alunosViewModel;
+    }
+
+    /// <summary>
+    /// Troca o CurrentPage para os Detalhes do aluno selecionado. Note que
+    /// SelectedNavigationItem NÃO muda aqui de propósito: a sidebar continua
+    /// a mostrar "Alunos" como ativo enquanto o utilizador vê os detalhes,
+    /// já que Detalhes do Aluno é uma sub-página de Alunos.
+    /// </summary>
+    private void AbrirDetalhesAluno(AlunoListItemModel aluno)
+    {
+        var detalhes = new DetalhesAlunoViewModel(aluno);
+
+        // "Voltar para Alunos" e "Excluir Aluno" (dentro de Detalhes) levam
+        // os dois de volta para uma lista de Alunos "fresca".
+        detalhes.VoltarParaAlunosSolicitado += (_, _) => CurrentPage = CriarPaginaAlunos();
+        detalhes.ExclusaoConfirmada += (_, _) => CurrentPage = CriarPaginaAlunos();
+
+        CurrentPage = detalhes;
     }
 
     partial void OnSelectedNavigationItemChanged(NavigationItemViewModel? value)
